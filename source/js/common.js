bd367ccec8145e7d4910fa9d01fde19529c976fb
rhkanrdl <go@footer.kr>
2023-04-25 16:54:13 +0900

    작업중

---
+++ source/js/common.js
@@ -0,0 +1,24 @@
+$(function () {
+
+
+
+  var swiper = new Swiper(".ticket-swiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+});
+
+
+
+/* ----- Window Size ----- */
+var windowh = $(window).height();
+var windowW = $(window).width();
+
+$(window).on('resize', function () {
+  windowh = $(window).height();
+  windowW = $(window).width();
+});
